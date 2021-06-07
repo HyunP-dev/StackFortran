f3fe0171de9ad9150cac61aac261a6787572f492
Hyun Park <pak_hyun@hallym.ac.kr>
2021-06-07 18:03:53 +0900

    Update test_stack_module.f90

--- test_stack_module.f90
+++ test_stack_module.f90
@@ -9,10 +9,19 @@ PROGRAM MAIN
     character :: temp_ch
 
     PRINT *, "TEST [Character Stack]"
-    CALL initiate(stk_char)
     
     print *, "> push 'a'"
     CALL push(stk_char, 'a')
+
+    write(*, fmt="(1x,a,i0)", advance="no") "("
+    do i=1, stk_char%top
+        write(*, fmt="(1x,a,i0)", advance="no") stk_char%items_array(i:i)
+        write(*, fmt="(1x,a,i0)", advance="no") " "
+    end do
+    print *, ")"
+
+    print *, "> push 'b'"
+    CALL push(stk_char, 'b')
 
     write(*, fmt="(1x,a,i0)", advance="no") "("
     do i=1, stk_char%top
